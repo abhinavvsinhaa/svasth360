61c1a0979196f90bf09c516efe3aaab3fd941be1
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-02-10 13:33:53 +0530

    updated axios config

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,7 +1,7 @@
 let _Environments = {
     production: {
-        BASE_URL: 'http://10.0.2.2:3005/v1/',
-        SOCKET_URL: 'http://localhost:3004/',
+        BASE_URL: 'http://44.201.226.198:3005/v1/',
+        SOCKET_URL: 'http://44.201.226.198:3004/',
         APP_ID: '231510712346de81',
         REGION: 'us',
         AUTH_KEY: 'bffdd621fbdcfd21a892ea3dae880eb33abf0073'
@@ -16,7 +16,7 @@ let _Environments = {
 }
 
 function getEnvironment() {
-    return _Environments.development
+    return _Environments.production
 }
 
 let env = getEnvironment()
